166d7cfa20cba688db31daadd9e97b45f6040da0
yezi <744177929@qq.com>
2018-12-31 21:18:48 +0800

    Initial commit

---
+++ src/main/java/org/yezi/netty/NettyServer.java
@@ -0,0 +1,43 @@
+package org.yezi.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
+
+public class NettyServer {
+    public static void main(String[] args) {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap
+                .group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                      protected void initChannel(NioSocketChannel ch) {
+                      }
+                });
+
+        bind(serverBootstrap, 1000);
+    }
+
+    // 自动绑定递增端口
+    private static void bind(final ServerBootstrap serverBootstrap, final int port) {
+        serverBootstrap.bind(port).addListener(new GenericFutureListener<Future<? super Void>>() {
+            @Override
+            public void operationComplete(Future<? super Void> future) throws Exception {
+                if(future.isSuccess()){
+                    System.out.println("端口[" + port + "]绑定成功");
+                }
+                else {
+                    System.out.println("端口[" + port + "]绑定失败");
+                    bind(serverBootstrap, port + 1);
+                }
+            }
+        });
+    }
+}
